7fe77d21ed5818cb7506378859ca74c687f1039b
Vr3n <vickyspatel@gmail.com>
2020-04-14 21:24:43 +0530

    Completed Basic version and Added Class Based View for list view

--- chapter1/mysite/blog/views.py
+++ chapter1/mysite/blog/views.py
@@ -1,26 +1,36 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 
 # Create your views here.
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in each page.
-    page = request.GET.get('page')  # Indicates the current page number.
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results.
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {
-        'posts': posts
-    })
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts in each page.
+#     page = request.GET.get('page')  # Indicates the current page number.
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page.
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results.
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {
+#         'posts': posts
+#     })
+
+class PostListView(ListView):
+    """
+    List View For Posts - lists all the published posts.
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
