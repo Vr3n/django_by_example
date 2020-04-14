1870fb64db854a3e22a225ba34398475dedc133a
Vr3n <vickyspatel@gmail.com>
2020-04-14 21:03:51 +0530

    Added Detail and List view with pagination

--- chapter1/mysite/blog/models.py
+++ chapter1/mysite/blog/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+
+# Publish Model Manager.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -12,15 +20,28 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()  # the default manager.
+    published = PublishedManager()  # the manager we created above.
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
