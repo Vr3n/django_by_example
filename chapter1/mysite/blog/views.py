8e356da54e1bb82a18109c72b8f04fa81efab304
Vr3n <vickyspatel@gmail.com>
2020-04-15 15:06:28 +0530

    Added Post sharing via Email Functionality

--- chapter1/mysite/blog/views.py
+++ chapter1/mysite/blog/views.py
@@ -1,9 +1,12 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
+import pdb
 # Create your views here.
 
 
@@ -38,4 +41,30 @@ def post_detail(request, year, month, day, post):
                              publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {
         'post': post
+    })
+
+
+def post_share(request, post_id):
+    # pdb.set_trace()
+    # Retieve post by id.
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation.
+            cleaned_data = form.cleaned_data
+            print(cleaned_data['email'])
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cleaned_data['name']} ({cleaned_data['email']} recommends you reading."
+            message = f"Read {post.title} at {post_url}\n\n{cleaned_data['name']}\'s comments: {cleaned_data['comments']}"
+            send_mail(subject, message, 'admin@myblog.com',
+                      [cleaned_data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {
+        'post': post, 'form': form, 'sent': sent
     })
