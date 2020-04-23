f4c1427516e0d860a35d83b852604aecc7a5680c
Vr3n <vickyspatel@gmail.com>
2020-04-23 14:05:04 +0530

    initialized images app and created model

--- Chapter2/bookmarks/bookmarks/settings.py
+++ Chapter2/bookmarks/bookmarks/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images',
 
     # THIRD PARTY APPS.
     'social_django',
