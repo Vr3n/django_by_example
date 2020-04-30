e949d318cfba49004d9434d11751550398864587
vr3n <vickyspatel@gmail.com>
2020-04-30 18:33:58 +0530

    Implemented Formsets for Modules

--- Chapter4/educa/courses/views.py
+++ Chapter4/educa/courses/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic.list import ListView
+from django.views.generic.base import TemplateResponseMixin, View, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
+from .forms import ModuleFormSet
 from .mixins import *
 from .models import Course
 # Create your views here.
@@ -24,3 +26,32 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView, PermissionRequiredMixin
 class CourseDeleteView(OwnerCourseMixin, DeleteView, PermissionRequiredMixin):
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset,
+        })
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset
+        })
