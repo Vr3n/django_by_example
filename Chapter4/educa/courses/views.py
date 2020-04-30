2bb3930f641ddc821d79468d0ea84c8026b39840
vr3n <vickyspatel@gmail.com>
2020-04-30 22:56:17 +0530

    Added List View and Detail view for courses which students or any user can see on home page

--- Chapter4/educa/courses/views.py
+++ Chapter4/educa/courses/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, \
     DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
@@ -9,11 +11,9 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from .models import Course
-from .models import Module, Content
+from .models import *
 from .mixins import *
 from .forms import ModuleFormSet
-
 
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
@@ -164,3 +164,26 @@ class ContentOrderView(CsrfExemptMixin,
                                    module__course__owner=request.user) \
                 .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses
+        })
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
