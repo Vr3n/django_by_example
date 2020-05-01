d16982563668c790e99f865ffbc245ed5b88314b
vr3n <vickyspatel@gmail.com>
2020-05-01 17:25:47 +0530

    Integrated REST API with serializers and router for Subjects, Course and Modules model.

---
+++ Chapter4/educa/courses/api/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from courses.models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+
+# Create your views here.
+
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class SubjectDetailView(generics.RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class CourseListView(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseDetailView(generics.RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+# class CourseEnrollView(APIView):
+#     authentication_classes = (BasicAuthentication, )
+#     permission_classes = (IsAuthenticated, )
+
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({
+#             'enrolled': True
+#         })
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @detail_route(
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response(({
+            'enrolled': True
+        }))
